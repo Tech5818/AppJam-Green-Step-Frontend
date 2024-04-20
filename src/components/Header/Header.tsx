6876810b4e76672131303f7024f591ed063143f3
Tech5818 <pqow5818@gmail.com>
2024-04-20 19:55:35 +0900

    :sparkles: header 완성

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,31 @@
 import styled from "styled-components";
 import { Mobile } from "../../Responsive";
+import { useCurrentUrl } from "../../store/CurrentUrl";
+import { Home } from "../svg/Home";
+import { Share } from "../svg/Share";
+import { Profile } from "../svg/Profile";
 
 export const Header = () => {
+  const { currentUrl } = useCurrentUrl();
+
+  const importIcon = () => {
+    if (currentUrl === "/") {
+      return <Home color="#5EC570" />;
+    } else if (currentUrl.includes("/community")) {
+      return <Share color="#5EC570" />;
+    } else if (currentUrl.includes("/profile")) {
+      return <Profile color="#5EC570" />;
+    } else {
+      return null;
+    }
+  };
+
   return (
     <>
       <Mobile>
         <Container>
           <Logo src="./img/header_logo.png" />
+          {importIcon()}
         </Container>
       </Mobile>
     </>
@@ -19,6 +38,7 @@ const Container = styled.div`
   display: flex;
   padding: 10px 20px;
   align-items: flex-end;
+  gap: 10px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.07);
 `;
 
